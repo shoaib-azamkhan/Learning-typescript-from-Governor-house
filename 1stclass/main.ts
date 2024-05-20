9eb06b1be3c80b0e1b74d3084164c76b4642160b
Shoaib_Azam8 <69983752+shoaib-azamkhan@users.noreply.github.com>
2024-05-20 20:14:08 +0000

    about variables and datatypes

---
+++ 1stclass/main.ts
@@ -0,0 +1,43 @@
+//console.log("hello world");
+let message  = "Hello World!";
+console.log(message);
+
+//variables:
+                  //let,const and var
+                  let myLetVariable = 'I am a let';
+                //we can change the value of let but can't change the value of const
+                //let and const are part of ES6 and both are braces scoped  i.e., they only exist within the cur
+                //var is not recommended to use in ES6 because it has function scope not block scope unlike let and or var and var
+            
+let firstName:string="shoaib";
+let age:number=21;
+let isMarried:boolean=false;
+
+console.log(`My name is ${firstName} and i am ${age} years old. am i married?${isMarried}`);
+
+let num1:number=5;
+let num2:number=2;
+let exponent=num1**num2;
+console.log(`The power of ${num1} ** ${num2} is: ${exponent}`);
+
+var userName:string="sher azam"; //yahan error nhi ayega jabki same username hai
+var userName:string="sher azam"; //yahan error nhi ayega jabki same username hai
+            //let userName:string="azam"; let say error aya
+            //let userName:string="nawaz";
+               
+            //functions:
+//---------------------------------
+// function addition(a :number,b:number){
+//   return(a+b);
+// }
+// let sum= addition(5,5);
+// console.log(sum);
+// //--------------------------------
+// function subtraction(user:number){
+//   console.log(8-3-user)
+// }
+
+// subtraction(2);
+
+
+
