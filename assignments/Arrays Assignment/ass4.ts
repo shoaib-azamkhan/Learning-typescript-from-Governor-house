f4b3be5e86694d56780e97577d85e0b37b084230
Shoaib_Azam8 <69983752+shoaib-azamkhan@users.noreply.github.com>
2024-05-30 05:31:29 +0000

    arrays assignment

---
+++ assignments/Arrays Assignment/ass4.ts
@@ -0,0 +1,61 @@
+// Question 1: Create an array named fruits that contains the following string
+// elements: "apple", "banana", "mango", "orange".
+
+let fruits :string[] = ["apple", "banana", "mango", "orange"];
+
+// Question 2: Declare an array named numbers that can contain only number
+// elements and initialize it with the values 10, 20, 30, and 40.
+
+let numbres:number[] = [10,20,30,40];
+
+// Question 3: Access the third element of the fruits array and assign it to a
+// variable named thirdFruit.
+
+let thirdFruit = fruits[2];
+console.log(thirdFruit);
+
+// Question 4: Change the second element of the numbers array to 25.
+
+numbres[1] = 25;
+
+// Question 5: Add the element "grape" to the end of the fruits array using the
+// method.
+
+fruits.push("grape");
+
+// Question 6: Remove the last element from the fruits array using the
+// method and assign it to a variable named lastFruit.
+
+let lastFruits = fruits.pop();
+
+// Question 7: Remove the first element from the fruits array using the
+// method and assign it to a variable named firstFruit.
+
+let firstFruit = fruits.shift();
+
+// Question 8: Add the element "kiwi" to the beginning of the fruits array
+// using the method.
+
+fruits.unshift("kiwi");
+
+// Question 9: Remove 2 elements from the fruits array starting from index 1
+// using the method.
+
+fruits.splice(1,2);
+
+// Question 10: Insert the elements "pineapple" and "pear" at index 2 of the
+// fruits array using the method.
+
+fruits.splice(2,0,"pinapple","pear");
+
+// Question 11: Create a new array named citrusFruits that contains the first
+// two elements of the fruits array using the method.
+
+let citrusFruits = fruits.slice(0,3);
+
+// Question 12: Create a new array named lastTwoFruits that contains the last
+// two elements of the fruits array using the method.
+
+let lastTwoFruits = fruits.slice(-2);
+
+// THE END
